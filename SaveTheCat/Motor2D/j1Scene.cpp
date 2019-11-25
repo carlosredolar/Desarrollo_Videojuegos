70a2b67b3653ec59d5a302fcb692ce381bda4719
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-11-25 16:21:26 +0100

    Comments
    Added comments to pathfinding steps

--- SaveTheCat/Motor2D/j1Scene.cpp
+++ SaveTheCat/Motor2D/j1Scene.cpp
@@ -86,7 +86,7 @@ bool j1Scene::PreUpdate()
 	iPoint p = App->render->ScreenToWorld(x, y);
 	p = App->map->WorldToMap(p.x, p.y);
 
-	if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN)
+	if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN)//Setting points and calculate path
 	{
 		if (origin_selected == true)
 		{
@@ -243,17 +243,19 @@ bool j1Scene::Update(float dt)
 
 	if (debugPath)
 	{
+		////////Mouse text render
 		int x, y;
 		App->input->GetMousePosition(x, y);
 		iPoint p = App->render->ScreenToWorld(x, y);
 		p = App->map->WorldToMap(p.x, p.y);
 		p = App->map->MapToWorld(p.x, p.y);
 
-		App->render->Blit(debug_tex, p.x, p.y);
-
+		App->render->Blit(debug_tex, p.x, p.y);///Text for mouse position
+		/////////////
+		
 		const p2DynArray<iPoint>* path = App->pathfinding->GetLastPath();
-
-		for (uint i = 0; i < path->Count(); ++i)
+		
+		for (uint i = 0; i < path->Count(); ++i)///Text for path
 		{
 			iPoint pos = App->map->MapToWorld(path->At(i)->x, path->At(i)->y);
 			App->render->Blit(debug_tex, pos.x, pos.y);
